cf6633a618831e59973c29cb5f5c16a1692c3d95
Yuzhuo Sun <Sun@dhcp-hol-2397.redrover.cornell.edu>
2015-10-18 21:21:09 -0400

    2nd version

--- src/BCNF.java
+++ src/BCNF.java
@@ -1,9 +1,18 @@
+/**
+ * BCNF decomposition
+ * CS5320 Homework3
+ * @author Yuzhuo Sun(ys684) Bangrui Chen(bc496) Jingyao Ren(jr986)
+ * @date Oct 18, 2015
+ */
 import java.util.*;
 
 public class BCNF {
 
   /**
-   * Implement your algorithm here
+   * Decompose the given relationship with BCNF
+   * @param Input attribute set
+   * @param Functional Dependencies
+   * @return BCNF decomposition wrapped into a set of attribute set 
    **/
   public static Set<AttributeSet> decompose(AttributeSet attributeSet,
           Set<FunctionalDependency> functionalDependencies) {
@@ -12,42 +21,51 @@ public class BCNF {
 	  return result;
   }
 	
-  public static void decomposeHelper(AttributeSet attributeSet,
+  /**
+   * Overwritten method of BCNF decomposition
+   * @param Input attribute set
+   * @param Functional Dependencies
+   * @param Result
+   **/
+  private static void decomposeHelper(AttributeSet attributeSet,
         Set<FunctionalDependency> functionalDependencies, Set<AttributeSet> result) {
 	int size = attributeSet.size();
 	List<Attribute> attributeList = new ArrayList<>();
-	
-	
 	
 	for(Iterator<Attribute> a = attributeSet.iterator(); a.hasNext();){
 		attributeList.add(a.next());
 	}
 	
 	for(int i = 1; i < Math.pow(2, size); i++){
+		// Assign temp to be the X in the algorithm given on the book
 		AttributeSet temp = new AttributeSet();
 		int k = i;
 		Iterator<Attribute> a;
-
+		
+		// Find the subset X in attributeSet
 		for(a = attributeSet.iterator(); a.hasNext();){
 			if(k % 2 == 1){
 				temp.addAttribute(a.next());
-			}
+			}else a.next();
 			k = k / 2;
 			if(k == 0) break;
 		}
 
+		// Compute closure
 		AttributeSet closure = closure(temp, functionalDependencies);
 		closure = intersect(closure, attributeSet);
+		// Check if X determines itself or if X is a super key
 		if((closure == null && temp == null) || (closure.size() == 0 && temp.size() == 0) || closure.equals(temp) || closure.equals(attributeSet)){
 			continue;
 		}else{
+			// If X does not hold, further decompose attributeSet
 			AttributeSet nonClosure = new AttributeSet();
-			for(int cnt = 0; cnt < attributeList.size(); cnt++){
-			}
+			// Compute X union (X+)^C
 			for(a = attributeSet.iterator(); a.hasNext();){
 				Attribute tempA = a.next();
 				if(!closure.contains(tempA) || temp.contains(tempA))	nonClosure.addAttribute(tempA);
 			}
+			// Recurse on both side
 			decomposeHelper(closure, functionalDependencies, result);
 			decomposeHelper(nonClosure, functionalDependencies, result);
 			return;
@@ -58,18 +76,20 @@ public class BCNF {
   
 
   /**
-   * Recommended helper method
+   * Compute the closure of a given attribute set
+   * @param Input attribute set
+   * @param Functional dependencies
+   * @return The closure of input attribute set
    **/
   public static AttributeSet closure(AttributeSet attributeSet, Set<FunctionalDependency> functionalDependencies) {
-    // TODO: implement me!
 	// initialization
 	Hashtable<FunctionalDependency, Integer> count = new Hashtable<>();
-	AttributeSet newdep = new AttributeSet(attributeSet); // *****************
-	AttributeSet update = new AttributeSet(attributeSet); // *****************
+	AttributeSet newdep = new AttributeSet(attributeSet); 
+	AttributeSet update = new AttributeSet(attributeSet); 
 	HashMap<Attribute, List<FunctionalDependency>> fdMap = new HashMap<>(); 
+
 	for (FunctionalDependency fd : functionalDependencies){
 		count.put(fd, fd.independent().size());
-		//List<FunctionalDependency> fdtemp = new ArrayList();
 		for (Iterator<Attribute> it = fd.independent().iterator(); it.hasNext();){
 			Attribute at = it.next();
 			if(fdMap.containsKey(at))	fdMap.get(at).add(fd);
@@ -81,26 +101,24 @@ public class BCNF {
 		}
 	}
 	
+	// Computation
 	while(!update.equals(null) && update.size() != 0){
+		// Choose an A in update
 		Attribute a = update.iterator().next();
-		//update.iterator().remove();
 		AttributeSet newUpdate = new AttributeSet();
+		// update: = update - a
 		for(Iterator<Attribute> it = update.iterator(); it.hasNext();){
 			Attribute temp = it.next();
 			if(temp != a)	newUpdate.addAttribute(temp);
 		}
+		
 		update = newUpdate;
+		
 		if(fdMap.containsKey(a)){
 			for(FunctionalDependency fd : fdMap.get(a)){
 				count.put(fd, count.get(fd) - 1);
 				if(count.get(fd) == 0){
-					AttributeSet add = new AttributeSet();
-					for(Iterator<Attribute> it = fd.dependent().iterator(); it.hasNext();){
-						Attribute at = it.next();
-						if(!newdep.contains(at)){
-							add.addAttribute(at);
-						}
-					}
+					AttributeSet add = minus(fd.dependent(), newdep);
 					union(add, newdep);
 					union(add, update);
 				}
@@ -110,7 +128,12 @@ public class BCNF {
 	  
     return newdep;
   }
-  
+   
+  /**
+   * Compute union between two attribute set
+   * @param Input attribute set A
+   * @param Input attribute set B, computed union saved here!
+   **/
   private static void union(AttributeSet a, AttributeSet b){
 	for(Iterator<Attribute> it = a.iterator(); it.hasNext();){
 		Attribute at = it.next();
@@ -120,11 +143,34 @@ public class BCNF {
 	}
   }
   
+  /**
+   * Compute intersect between two attribute set
+   * @param Input attribute set A
+   * @param Input attribute set B
+   * @return Output intersected attribute
+   **/
   private static AttributeSet intersect(AttributeSet a, AttributeSet b){
 	AttributeSet result = new AttributeSet();
 	for(Iterator<Attribute> it = a.iterator(); it.hasNext();){
 		Attribute at = it.next();
 		if(b.contains(at)){
+			result.addAttribute(at);
+		}
+	}
+	return result;
+  }
+  
+  /**
+   * Compute minus between two attribute set
+   * @param Input attribute set A
+   * @param Input attribute set B
+   * @return Output minus attribute
+   **/
+  private static AttributeSet minus(AttributeSet a, AttributeSet b){
+	AttributeSet result = new AttributeSet();
+	for(Iterator<Attribute> it = a.iterator(); it.hasNext();){
+		Attribute at = it.next();
+		if(!b.contains(at)){
 			result.addAttribute(at);
 		}
 	}
--- src/BCNFTest.java
+++ src/BCNFTest.java
@@ -40,4 +40,38 @@ public class BCNFTest {
     }
 
   }
+  
+  @Test
+  public void testComplicatedBCNF() {
+    //construct table
+    AttributeSet attrs = new AttributeSet();
+    attrs.addAttribute(new Attribute("a"));
+    attrs.addAttribute(new Attribute("b"));
+    attrs.addAttribute(new Attribute("c"));
+    attrs.addAttribute(new Attribute("d"));
+    attrs.addAttribute(new Attribute("e"));
+
+    //create functional dependencies
+    Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+    AttributeSet ind1 = new AttributeSet();
+    AttributeSet dep1 = new AttributeSet();
+    ind1.addAttribute(new Attribute("a"));
+    dep1.addAttribute(new Attribute("b"));
+    FunctionalDependency fd1 = new FunctionalDependency(ind1, dep1);
+    fds.add(fd1);
+    
+    AttributeSet ind2 = new AttributeSet();
+    AttributeSet dep2 = new AttributeSet();
+    ind2.addAttribute(new Attribute("c"));
+    dep2.addAttribute(new Attribute("d"));
+    FunctionalDependency fd2 = new FunctionalDependency(ind2, dep2);
+    fds.add(fd2);
+
+    //run client code
+    Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+    //verify output
+    assertEquals("Incorrect number of tables", 3, bcnf.size());
+
+  }
 }
